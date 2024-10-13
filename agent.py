a5f5cb0d9d13dfd439efef9c9729e4d2852fc64d
Gerrit-ba <50711973+SwagLag@users.noreply.github.com>
2024-10-13 14:55:19 +0200

    Merged nlp_utils and agent_main into nlp and agent respectively
    llm_utils left as is, please review before removing any files if you desire, but know you can see old files with github historic view.

--- agent.py
+++ agent.py
@@ -2,7 +2,10 @@
 # TODO: Alternatively we can use Mesa which was mentioned in the slides. I used this before
 # to make agent simulations work, but it's more focused around getting metrics and spatial
 # simulations. The former might be of use here though.
-from typing import List
+from typing import List, Dict
+
+from llm_utils import initialize_llm, generate_synonyms
+from nlp import remo
 
 class Prompt:
     """Used to wrap a prompt given to the environment, to be processed by an agent."""
@@ -33,20 +36,55 @@ class Env:
     def add_agent(self, a):
         self.agents.append(a)
 
+    def add_prompt(self, prompt: Prompt):
+        self.prompts.append(prompt)
+
+    def add_source(self, source: Source):
+        self.sources.append(source)
+
+    def reset(self):
+        self.prompts = []
+        self.sources = []
+
 class Agent:
     """Contains all information related to individual agents."""
+    # TODO: Massive work in progress, needs more logic.
     def __init__(self, env: Env):
-        self.env = env
+        llm, output_parser = initialize_llm()
+        self.llm = llm
+        self.output_parser = output_parser
+        
+        self.env : Env = env
+        self.prompt : Prompt = None
+        self.variedprompt : List[str] = []
+        self.source : Source = None
+        self.vars = []  # In which we will store truth values. Maybe a dictionary with key/val; source/truth value?
+
         self.id = len(env.agents)
+        self.state = 1
+
+        self.answer = ""
+        self.explanation = ""
     
     def perceive(self):
-        pass
+        if self.state == 1:  # Get a prompt from an user in the env.
+            if len(self.env.prompts) > 0:
+                self.prompt = self.env.prompts.pop(0)
+        elif self.state == 3:  # Get a external source from the env.
+            self.source = self.env.sources.pop(0)
+
 
     def reason(self):
-        pass
+        if self.state == 1:
+            if isinstance(self.prompt, Prompt):
+                self.variedprompt = generate_synonyms(self.llm, self.prompt, 2)
+                self.state = 2
 
     def act(self):
-        pass
+        if self.state == 5:
+            print(self.answer)
+            print(self.explanation)
+        # Perhaps querying the ontology is a valid action?
 
     def __str__(self):
         return "AGENT \n - ID: {}".format(self.id)
--- nlp.py
+++ nlp.py
@@ -1,14 +1,66 @@
 """NLP Module used to turn prompts into queries."""
 import nltk
+import string
 from nltk.util import ngrams
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from typing import List
 
-query_db = ["Are frogs dangerous?", "Can sharks be found on land?", "Are frogs not dangerous?"]
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger_eng')
+
+# query_db = ["Are frogs dangerous?", "Can sharks be found on land?", "Are frogs not dangerous?"]
 
 """Remember; queries usually involve selecting a domain first (MedicalConditions, Food, etc.) and
 then querying via a relationship. Quite limited but it will have to do for starters."""
 
-def prompt_parser(prompt: str) -> str:
-    """Turns a prompt into a DL Query for use in the OWL API for determining whether a statement is truthful."""
+def remove_stopwords(tokens: List[str]):
+    """
+        This function receives a sentence and removes its stopwords and punctuation.  
+    Args:
+        tokens (List[str]): a list of tokens preprocessed by nltk.tokenize(str)
+        
+    Returns:
+        filtered_words (List[str]): A list of the words of the original sentence after the cleaning
+    """
+    stop_words = set(stopwords.words('english'))
+    punctuation = set(string.punctuation)
+    # word_tokens = word_tokenize(sentence)
+    filtered_words = [word for word in tokens if word.lower() not in stop_words and word not in punctuation]
+    # TODO: Can later remove word.lower() as this should already be done in the process by this point.
+    # print(tokens)
+    # print(filtered_words)
+    
+    return filtered_words
+
+
+def pos_tagging(words):
+    """_summary_
+
+    Args:
+        words (list or string): A list of words or a sentence
+
+    Returns:
+        pos_tags (list): A list of tuples (Word, TAG)
+    """
+    
+    if type(words) is not list:
+        word_tokens = word_tokenize(words)
+    else:
+        word_tokens = words
+        
+    pos_tags = nltk.pos_tag(word_tokens)
+    print(pos_tags)
+    
+    return pos_tags
+
+def prompt_parser(tokens: List[str]) -> str:
+    """Turns a tokenized prompt into a DL Query for use in the OWL API for determining whether a statement is truthful."""
+    end_query = ""
+    # Step 1: Determine relevant sets (similarity)
+    # Step 2: Determine relevant relationship
+    return end_query
 
 def filter(pre: str, blacklist : list =[":",";",",",".","?","!"]):
     new = ""
@@ -23,7 +75,11 @@ def extract_negations(tokens: list) -> list:
 
 def preprocess_text(prompt: str) -> str:
     """Preprocesses a text for NLP usage."""
+    # Step 0; lowercase the entire string
+    substr0 = prompt.lower()
     # Step 1; remove punctuation from entire string.
-    substr1 = filter(prompt)
+    substr1 = filter(substr0)
     # Step 2; tokenize
-    substr2 = nltk.tokenize(substr1)
+    tokens1 = nltk.tokenize(substr1)
+    # Step 3; remove stopwords(?; some of these words we may need to keep for negations)
+    tokens2 = remove_stopwords(tokens1)
