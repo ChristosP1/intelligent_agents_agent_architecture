5cc79606aff37853d0dec4128aa3d1034c060ae8
Saskia Breit <saskiab@live.nl>
2024-10-24 16:32:24 +0200

    Converted all queries into SPARQL

--- OWL_interface/OWL_interface.py
+++ OWL_interface/OWL_interface.py
@@ -8,16 +8,19 @@ class OWLInterface:
         self.ontology = get_ontology(ontology_path).load()
 
         with self.ontology:
-            sync_reasoner(infer_property_values=True)
+            sync_reasoner_pellet(infer_property_values = True, infer_data_property_values = True)
 
 
-    def query_ontology(self, query):
+    def query_ontology(self, queries):
         # Print all instances of Symptom and their resultsFrom property
         # print(self.ontology.search(is_a=self.ontology.Symptom, resultsFrom=self.ontology.search(is_a=self.ontology.Concussion)))
 
-        instances = list(default_world.sparql(query))
-        print(instances)
-        result = [instance[0].name for instance in instances]
-        if result: result.insert(0,"True")
-        if not result: result.insert(0, "False")
-        return result
+        results = []
+        for query in queries:
+            instances = list(default_world.sparql(query))
+            result = [instance[0].name for instance in instances]
+            if result: result.insert(0,"True")
+            if not result: result.insert(0, "False")
+            results.append(result)
+
+        return results
--- OWL_interface/main.py
+++ OWL_interface/main.py
@@ -5,65 +5,144 @@ def main():
     ontology_path = os.getcwd()+"\\ontology3.owl"
     owl_interface = OWLInterface(ontology_path)
 
-    sparql_query_1 = """
+    # One example query unrelated to any of our scenarios
+    example_query = ["""
             PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
-
-            SELECT ?individual
+        
+            SELECT ?recipe
             WHERE {
-                ?individual a ex:FrogAndToad .
-                ?individual ex:isPoisonous false .
+              ?recipe a ex:Recipe;
+                    ex:hasIngredient ex:Cheese .
+            }
+        """]
+    # results = owl_interface.query_ontology(example_query)
+    # print(results)
+
+    scenario1 = [
+            """
+            PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
+            
+            SELECT ?condition
+            WHERE {
+              ?condition a ex:Dehydration;
+                    ex:hasSymptom ex:Headache;
+                    ex:shouldVisitDoctor false .
             }
             """
-    # result = owl_interface.query_ontology(sparql_query_1)
-    # print("Result for query 1: ",result)
+        ,
+            """
+            PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
+            
+            SELECT ?injury
+            WHERE {
+              ?injury a ex:Injury;
+                    ex:isCausedBy ex:Football;
+                    ex:hasSymptom ex:Headache .
+            }
+            """
+        ,
+            """
+            PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
 
-    sparql_query_2 = """
-        PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
+            SELECT ?injury
+            WHERE {
+              ?injury a ex:Injury;
+                    ex:hasSymptom ex:Headache;
+                    ex:shouldVisitDoctor true .
+            }
+            """
+        ,
+            """
+            PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
 
-        SELECT ?symptom
-        WHERE {
-          ?symptom a ex:Symptom .
-          ?symptom ex:resultsFrom ex:Concussion .
-        }
-    """
-    # results_2 = owl_interface.query_ontology(sparql_query_2)
-    # print("Results for Query 2:", results_2)
-
-    spark_sql_queries = [
-        # Query 1: Dehydration and (hasSymptom some {Headache}) and (shouldVisitDoctor value true)
-        """
-        SELECT * 
-        FROM conditions 
-        WHERE condition_name = 'Dehydration' 
-        AND hasSymptom LIKE '%Headache%' 
-        AND shouldVisitDoctor = true;
-        """,
-
-        # Query 2: Injury and (isCausedBy some {Football}) and (hasSymptom some {Headache})
-        """
-        SELECT * 
-        FROM injuries 
-        WHERE isCausedBy LIKE '%Football%' 
-        AND hasSymptom LIKE '%Headache%';
-        """,
-
-        # Query 3: Injury and (hasSymptom some {Headache}) and (shouldVisitDoctor value true)
-        """
-        SELECT * 
-        FROM injuries 
-        WHERE hasSymptom LIKE '%Headache%' 
-        AND shouldVisitDoctor = true;
-        """,
-
-        # Query 4: Symptom and (resultsFrom some {Concussion})
-        """
-        SELECT * 
-        FROM symptoms 
-        WHERE resultsFrom LIKE '%Concussion%';
-        """
+            SELECT ?symptom
+            WHERE {
+              ?symptom a ex:Symptom;
+                    ex:resultsFrom ex:Concussion .
+            }
+            """
     ]
-    for query in spark_sql_queries:
-        print(owl_interface.query_ontology(query))
+    results = owl_interface.query_ontology(scenario1)
+    print(results)
+    print("Expected results: -, Concussion, Concussion, Amnesia Fatigue Headache Nausea\n")
+
+    scenario2 = [
+            """
+            PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
+    
+            SELECT ?frogAndToad
+            WHERE {
+              ?frogAndToad a ex:FrogAndToad;
+                           ex:isPoisonous false;
+                           ex:isEatenBy ex:Human .
+            }
+            """
+        ,
+            """
+            PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
+
+            SELECT ?nutrient
+            WHERE {
+              ?nutrient a ex:Nutrient;
+                        ex:isPresentIn ex:FrogAndToad .
+            }
+            """
+        ,
+            """
+            PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
+
+            SELECT ?recipe
+            WHERE {
+              ?recipe a ex:Recipe;
+                      ex:hasIngredient ex:FrogAndToad .
+            }
+            """
+    ]
+    results = owl_interface.query_ontology(scenario2)
+    print(results)
+    print("Expected results: Bullfrog, magnesium omega3 phosphorus selenium vitaminB, friedfroglegs frogsoup\n")
+
+    scenario3 = [
+            """
+            PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
+            
+            SELECT ?location
+            WHERE {
+              ?location a ex:Location;
+                         ex:hasAnimal ex:Shark;
+                         ex:hasSport ex:Volleyball .
+            }
+            """
+        ,
+            """
+            PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
+
+            SELECT ?vegetarian
+            WHERE {
+              ?vegetarian a ex:Vegetarian;
+                           ex:hasIngredient ?ingredient;
+                           ex:isEatenBy ex:Shark .
+              FILTER (?ingredient = ex:Animal || ?ingredient = ex:Seafood || ?ingredient = ex:Meat)
+            }
+            """
+        ,
+            """
+            PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+            
+            SELECT ?sport
+            WHERE {
+              ?sport a ex:Swimming ;
+                     a ex:RepetitiveNonContact ;
+                     ex:caloriesBurnedPerHour ?calories .
+              FILTER(?calories > 500)
+            }
+            """
+    ]
+    results = owl_interface.query_ontology(scenario3)
+    print(results)
+    print("Expected results: -, -, swimming\n")
+
 
 if __name__ == "__main__":
     main()
