652af089b1ac484139b2acfab872e5e1f0c7016c
Saskia Breit <saskiab@live.nl>
2024-10-24 10:11:07 +0200

    sql

--- OWL_interface/OWL_interface.py
+++ OWL_interface/OWL_interface.py
@@ -7,104 +7,17 @@ class OWLInterface:
     def __init__(self, ontology_path):
         self.ontology = get_ontology(ontology_path).load()
 
-        # Run reasoner to obtain inferences
         with self.ontology:
             sync_reasoner_pellet(infer_property_values=True)
 
-        # Additional
-        # Reference dictionaries between IRIs and given labels that might be useful
-        self.label_to_class = {ent.label[0]: ent for ent in self.ontology.classes()}
-        self.label_to_prop = {prop.label[0]: prop for prop in self.ontology.properties()}
 
-        self.class_to_label = {ent: ent.label[0] for ent in self.ontology.classes()}
-        self.prop_to_label = {prop: prop.label[0] for prop in self.ontology.properties()}
+    def query_ontology(self, query):
+        # Print all instances of Symptom and their resultsFrom property
+        # print(self.ontology.search(is_a=self.ontology.Symptom, resultsFrom=self.ontology.search(is_a=self.ontology.Concussion)))
 
-        # Save types to help differentiate between classes and properties later on
-        self.class_type = type(list(self.ontology.classes())[0])
-        self.property_type = type(list(self.ontology.properties())[0])
-
-
-    # def query_ontology_first_iteration(self):#, query):
-    #     # print("The classes included in the ontology are:")
-    #     # print([cls.name for cls in self.ontology.classes()])
-    #
-    #     frog_and_toad = self.ontology.FrogAndToad
-    #     results = [instance for instance in frog_and_toad.instances() if instance.isPoisonous == [False]]
-    #
-    #     return results # Returns: [C:\Users\saski\Documents\GitHub\intelligent_agents_agent_architecture\OWL_interface\ontology3.Bullfrog]
-
-
-    def query_ontology_second_iteration(self, query):  # , query):
-
-        query_class, query_condition = self.parse_query(query)
-        # If there's no condition, return all instances of the class
-        if not query_condition:
-            return list(query_class.instances())
-
-        property_type, query_property, query_instance, value = self.parse_condition(query_condition)
-
-        if property_type == "object_property":
-            # Search for instances of the class that have the property related to the instance
-            results = [
-                instance.name for instance in query_class.instances()
-                if query_instance in getattr(instance, query_property.python_name, [])
-            ]
-
-        elif property_type == "data_property":
-            results = [
-                instance.name for instance in query_class.instances()
-                if getattr(instance, query_property.python_name) == [value]
-            ]
-
-        return results
-
-
-    def parse_query(self, query):
-        query_parts = query.split(" and ")
-
-        # Get class
-        class_name = query_parts[0].strip()
-        query_class = self.label_to_class.get(class_name)
-        if not query_class:
-            raise ValueError(f"Class '{class_name}' not found in the ontology.")
-
-        # Get condition
-        condition = query_parts[1].strip("()").strip() if len(
-            query_parts) > 1 else None
-
-        return query_class, condition
-
-
-    def parse_condition(self, condition):
-        property_type = None
-        property_name = None
-        query_instance = None
-        value = None
-
-        if "some" in condition:
-            property_type = "object_property"
-            prop_name, instance_name = condition.split(" some ")
-            property_name = prop_name.strip()
-            instance_name = instance_name.strip("{}").strip()
-            query_instance = self.ontology.search_one(label=instance_name)
-
-        elif "value" in condition:
-            property_type = "data_property"
-            prop_name, value_str = condition.split(" value ")
-            property_name = prop_name.strip()
-            value = self.parse_value(value_str)
-
-        query_property = self.label_to_prop.get(property_name)
-
-        # if not query_property:
-        #     raise ValueError(f"Property '{property_name}' not found in the ontology.")
-        # if not query_instance:
-        #     raise ValueError(f"Instance '{instance_name}' not found in the ontology.")
-
-        return property_type, query_property, query_instance, value
-
-    def parse_value(self, string):
-        if string == "true":
-            return True
-        if string == "false":
-            return False
+        instances = list(default_world.sparql(query))
+        print(instances)
+        result = [instance[0].name for instance in instances]
+        if result: result.insert(0,"True")
+        if not result: result.insert(0, "False")
+        return result
--- OWL_interface/main.py
+++ OWL_interface/main.py
@@ -5,24 +5,30 @@ def main():
     ontology_path = os.getcwd()+"\\ontology3.owl"
     owl_interface = OWLInterface(ontology_path)
 
-    # result = owl_interface.query_ontology_first_iteration()
-    # print(f"Result of the query: {result}")
+    sparql_query_1 = """
+            PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
 
-    example_query1 = "FrogAndToad and (isPoisonous value false)" # Result should be instance Bullfrog
-    example_query2 = "Symptom and (resultsFrom some {Concussion})" # Result should be instances: Amnesia, Fatigue, Headache, Nausea
-    result = owl_interface.query_ontology_second_iteration(example_query1)
-    print(f"Result of the query:{example_query1} {result}")
-    result = owl_interface.query_ontology_second_iteration(example_query2)
-    print(f"Result of the query:{example_query2} {result}")
+            SELECT ?frog
+            WHERE {
+                ?frog rdf:type ex:FrogAndToad .
+                ?frog ex:isPoisonous false .
+            }
+            """
+    result = owl_interface.query_ontology(sparql_query_1)
+    print("Result for query 1: ",result)
 
+    sparql_query_2 = """
+        PREFIX ex: <http://www.semanticweb.org/chris/ontologies/2024/8/intelligent_agents_ontology#>
+        SELECT ?symptom
+        WHERE {
+            ?symptom a ex:Symptom .
+            ?symptom ex:resultsFrom ?concussion.
+            FILTER(?concussion = ex:Concussion)
+        }
+    """
+    results_2 = owl_interface.query_ontology(sparql_query_2)
+    print("Results for Query 2:", results_2)
 
-
-
-    # Function to be used in our final agent class
-    # result = owl_interface.query_ontology(example_query1)
-
-    # result = owl_interface.query_ontology(example_query2)
-    # print(f"Result of the query: {result}")
 
 if __name__ == "__main__":
     main()
